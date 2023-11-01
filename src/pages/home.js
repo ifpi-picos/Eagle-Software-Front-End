404ba14c4899c70cffb92439591a378bf9632e25
Renanlucass <renanlucas0014@gmail.com>
2023-11-01 10:54:34 -0300

    Correções no Projeto

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,9 +3,8 @@ import Sidebar from '../components/sidebar/menu'
 import Busca from '../components/busca/search'
 import styles from '../components/sidebar/menu.module.css'
 import { getCookie } from 'cookies-next'
-import { verificaToken } from "../services/user"
 
-export default function Home (){
+export default function Home() {
     return (
         <div className={styles['body-home']}>
             <div className={styles['listaItens']}>
@@ -16,22 +15,18 @@ export default function Home (){
     );
 }
 
-export const getServerSideProps = async ({req, res}) => {
-    try{
-        const token = getCookie('authorization', {req, res})
+export const getServerSideProps = async ({ req, res }) => {
+    try {
+        const token = getCookie('authorization', { req, res })
         console.log(token)
-        if (!token) throw new Error ('Token Inválido')
+        if (!token) throw new Error('Token Inválido')
 
         verifica(token)
-        return{
+        return {
             props: {}
         }
-    }catch(err){
-        return{
-            // redirect:{
-            //     permanent: false,
-            //     destination: '/login'
-            // },
+    } catch (err) {
+        return {
             props: {}
         }
     }
