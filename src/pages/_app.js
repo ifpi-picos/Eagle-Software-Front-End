98edcc7c3ce991b9cbf73673dbb629ca9455aa28
Renanlucass <renanlucas0014@gmail.com>
2025-02-26 13:33:27 -0300

    Adicionando icones ao navegador

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -6,6 +6,11 @@ function MyApp({ Component, pageProps }) {
     <>
       <Head>
         <title>Eagles Software</title>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="robots" content="index, follow" />
+        <meta name="keywords" content="tecnologia, suporte, eficiência, plataforma" />
+        <meta name="description" content="Eagles Software: A solução ideal para encontrar objetos perdidos com rapidez e eficiência." />
       </Head>
       <Component {...pageProps} />
     </>
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -9,15 +9,11 @@ class MyDocument extends Document {
                     <meta name="viewport" content="width=device-width, initial-scale=1" />
                     <meta name="robots" content="index, follow" />
                     <meta name="keywords" content="tecnologia, suporte, eficiência, plataforma" />
-                    <meta
-                        name="description"
-                        content="Descubra os benefícios da nossa plataforma: compatibilidade, eficiência e suporte completo para sua equipe."
-                    />
-
-                    <link rel="icon" type="image/svg+xml" href="/logotipo.svg" />
-                    <link rel="icon" type="image/png" sizes="32x32" href="/logotipo.svg" />
-                    <link rel="icon" type="image/png" sizes="16x16" href="/logotipo.svg" />
-                    <link rel="apple-touch-icon" sizes="180x180" href="/logotipo.svg" />
+                    <meta name="description" content="Eagles Software: A solução ideal para encontrar objetos perdidos com rapidez e eficiência." />
+                    <link rel="icon" href="/favicon.ico"/>
+                    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+                    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+                    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
                 </Head>
                 <body>
                     <Main />
